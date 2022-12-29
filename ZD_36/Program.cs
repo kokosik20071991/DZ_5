3427f31d056adf4af57313ad35397ae02b080d19
Viktoria <vikatim4encko@yandex.ru>
2022-12-29 13:36:32 +0300

    Задача 36

---
+++ ZD_36/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Массив: ");
+PrintArray(numbers);
+int sum = 0;
+
+for (int x = 0; x < numbers.Length; x+=2)
+    sum = sum + numbers[x];
+
+    Console.WriteLine($"Сумма элементов на нечётных позициях = {sum}");
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = new Random().Next(1,10);
+        }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
