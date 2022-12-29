c14a9d1d4c72baf6d1d3c9db3c3749fbe5a3bb54
Viktoria <vikatim4encko@yandex.ru>
2022-12-29 09:45:04 +0300

    Задача 34

---
+++ ZD_34/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array=new int[7];
+
+for (int i=0; i<array.Length; i++)
+{
+    array[i]=new Random().Next(100,1000);
+    Console.Write($"{array[i]} ");
+}
+Console.WriteLine();
+int k=0;
+for (int i=0; i<array.Length; i++)
+{if(array[i]%2==0)
+ k++;
+
+}
+
+Console.Write($"В массиве количество четных чисел равно: {k}");
